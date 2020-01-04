47d875f62b8e7d1546e22c84c4de3bb1be6f0a3f
BleronMexhuani <58087682+BleronMexhuani@users.noreply.github.com>
2020-01-05 00:18:12 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,8 @@ const size = Math.round(canvas.width / 50);
 const xEnd = Math.round(canvas.width / size) * size;
 const yEnd = Math.round(canvas.height / size) * size;
 let directionLock = false;
+
+const snake = [{x: 0, y: 0}];
+const apple = {};
+let direction = 'right';
+let speed = 200;
