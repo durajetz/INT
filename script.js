3420c690bd5aa467819254c49a0eeaf9e9ce0b8d
BleronMexhuani <58087682+BleronMexhuani@users.noreply.github.com>
2020-01-05 00:21:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -35,4 +35,63 @@ function draw() {
 
   window.requestAnimationFrame(draw);
 }
+function tick() {
+  for (let i=snake.length-1; i>=0; i--) {
+    if (i === 0 && snake[i].x === apple.x && snake[i].y === apple.y) {
+      snake.push({});
+      speed *= 0.99;
+      setApple();
+    }
 
+    const s = snake[i];
+    if (i == 0) {
+      switch(direction) {
+        case 'right':
+          if (s.x > canvas.width) s.x = 0;
+          else s.x += size;
+          break;
+        case 'down':
+          if (s.y > canvas.height) s.y = 0;
+          else s.y += size;
+          break;
+        case 'left':
+          if (s.x < 0) s.x = xEnd;
+          else s.x -= size;
+          break;
+        case 'up':
+          if (s.y < 0) s.y = yEnd;
+          else s.y -= size;
+      }
+
+      for (let j = 1; j < snake.length; j += 1) {
+        if (snake[0].x === snake[j].x && snake[0].y === snake[j].y) {
+          alert('GAME OVER');
+          window.location.reload();
+        }
+      }
+
+    } else {
+      snake[i].x = snake[i-1].x;
+      snake[i].y = snake[i-1].y;
+    }
+  }
+  window.setTimeout(tick, speed);
+  directionLock = false;
+}
+
+function onKeyDown(e) {
+  if (!directionLock) {
+    directionLock = true;
+    const newDirection = e.key.substr(5).toLowerCase();
+
+    if (direction === 'left' && newDirection !== 'right') direction = newDirection;
+    if (direction === 'up' && newDirection !== 'down') direction = newDirection;
+    if (direction === 'down' && newDirection !== 'up') direction = newDirection;
+    if (direction === 'right' && newDirection !== 'left') direction = newDirection;
+  }
+}
+
+setApple();
+window.addEventListener('keydown', onKeyDown);
+window.setTimeout(tick, speed);
+window.requestAnimationFrame(draw);
